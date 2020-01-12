7fe5d770c93b6b2cb95e14a86cef07a3a6da51ad
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2020-01-12 22:19:10 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -95,7 +95,9 @@ def apply_coupons(cart, coupons)
         else
           cart_item_with_coupon = {
             :item => couponed_item_name,
-            :price => coupons[counter][:cost] / coupons[counter][:num]
+            :price => coupons[counter][:cost] / coupons[counter][:num],
+            :count => coupons[counter][:num],
+            :clearance => cart_item
           }
         end
     end
