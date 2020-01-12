ad71022e762d7b269db35581d24da29051f7441b
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2020-01-12 22:38:54 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -117,7 +117,7 @@ def apply_clearance(cart)
   counter = 0 
   while counter < cart.lenght 
     if cart[counter][:clearance]
-      cart[counter][:price] = (cart[counter][:price] - (cart[counter][:price] *0.20).float
+      cart[counter][:price] = (cart[counter][:price] - (cart[counter][:price] *0.20).round(2)
     end
   counter += 1 
   
