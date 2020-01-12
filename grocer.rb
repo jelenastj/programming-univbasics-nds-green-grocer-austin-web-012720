f36ea23b973e405a70d9b4af5bb851ff34c43b59
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2020-01-12 22:10:05 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -86,8 +86,12 @@ def apply_coupons(cart, coupons)
   while counter < coupons.length 
   cart_item =  find_item_by_name_in_collection(coupons[counter][:item], cart)
   couponed_item_name = "#{coupons[counter][:item]} W/COUPON"
-  counter += 1 
   cart_item_with_coupon = find_item_by_name_in_collection (couponed_item_name, cart)
+  
+    if cart_item
+  
+  counter += 1 
+  
 end
  
  
