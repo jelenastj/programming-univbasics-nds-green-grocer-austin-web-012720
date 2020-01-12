3bee8b225e50c473a486e1e9f4b83ae508eafedb
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2020-01-12 22:13:43 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -90,7 +90,7 @@ def apply_coupons(cart, coupons)
   
     if cart_item && cart_item[:count] >= coupons[counter][:num]
         if cart_item_with_coupon 
-          cart_item_with_coupon[:count] 
+          cart_item_with_coupon[:count] +=
           
         end
     end
